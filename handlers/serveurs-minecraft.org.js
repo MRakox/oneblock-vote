b3ccda3e8772832b1da0373b825558ca1ef051cf
MRakox <rakoxwastaken@gmail.com>
2022-07-22 03:11:47 +0200

    🔥 Remove google recaptcha solver

--- handlers/serveurs-minecraft.org.js
+++ handlers/serveurs-minecraft.org.js
@@ -1,14 +1,8 @@
-// import solve from '../solve.js';
 import { TIMEOUT } from '../constants.js';
 import { randomClickDelay } from '../utils.js';
 
 /** @param {import('puppeteer').Page} page */
 export default async function handle(page) {
-  // Wait for the captcha to be solved
-  // await solve(page).catch((err) => {
-  //   throw new Error(`Unable to solve the captcha in time: ${err.message}`);
-  // });
-
   // Wait for the page to load
   await page.waitForTimeout(TIMEOUT * 5);
 
