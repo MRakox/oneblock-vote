fb385856176d8e687834a97d1d4e401cf1230215
MRakox <rakoxwastaken@gmail.com>
2022-07-23 00:43:23 +0200

    🐛 Take a screenshot of the full page

--- handlers/top-serveurs.net.js
+++ handlers/top-serveurs.net.js
@@ -6,7 +6,7 @@ export default async function handle(page) {
   // Wait for the page to load
   await page.waitForTimeout(TIMEOUT * 10);
 
-  await page.screenshot({ path: 'screenshots/top-serveurs.net.png' });
+  await page.screenshot({ path: 'screenshots/top-serveurs.net.png', fullPage: true });
 
   // Submit the vote form
   await page.click('.btn-submit-vote', { delay: randomClickDelay() });
