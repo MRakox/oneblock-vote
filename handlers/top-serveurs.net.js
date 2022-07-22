7e785a04b5bc72fc73aa773f1fd06e5842d249f0
MRakox <rakoxwastaken@gmail.com>
2022-07-23 00:46:50 +0200

    🔊 Add debug logs

--- handlers/top-serveurs.net.js
+++ handlers/top-serveurs.net.js
@@ -6,8 +6,12 @@ export default async function handle(page) {
   // Wait for the page to load
   await page.waitForTimeout(TIMEOUT * 10);
 
-  await page.screenshot({ path: 'screenshots/top-serveurs.net.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/top-serveurs.net-before.png', fullPage: true });
 
   // Submit the vote form
   await page.click('.btn-submit-vote', { delay: randomClickDelay() });
+
+  // * DEBUG:
+  await page.waitForTimeout(TIMEOUT * 5);
+  await page.screenshot({ path: 'screenshots/top-serveurs.net-after.png', fullPage: true });
 }
