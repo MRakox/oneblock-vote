dcc9b4dced6174808e377da7d34da195747109fc
MRakox <rakoxwastaken@gmail.com>
2022-07-22 03:09:59 +0200

    🐛 Wait for captcha challenge to load

--- handlers/top-serveurs.net.js
+++ handlers/top-serveurs.net.js
@@ -1,14 +1,16 @@
 // import solve from '../solve.js';
+import { TIMEOUT } from '../constants.js';
 import { randomClickDelay } from '../utils.js';
 
 /** @param {import('puppeteer').Page} page */
 export default async function handle(page) {
-  page.setDefaultNavigationTimeout(0);
-
   // Wait for the captcha to be solved
   // await solve(page).catch((err) => {
   //   throw new Error(`Unable to solve the captcha in time: ${err.message}`);
   // });
+
+  // Wait for the page to load
+  await page.waitForTimeout(TIMEOUT * 5);
 
   // Submit the vote form
   await page.click('.btn-submit-vote', { delay: randomClickDelay() });
