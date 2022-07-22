58ca56495a8014f9179a0473a4903bb3285003ff
MRakox <rakoxwastaken@gmail.com>
2022-07-22 14:01:55 +0200

    🐛 Fix timeout delay

--- handlers/top-serveurs.net.js
+++ handlers/top-serveurs.net.js
@@ -4,7 +4,7 @@ import { randomClickDelay } from '../utils/random.js';
 /** @param {import('puppeteer').Page} page */
 export default async function handle(page) {
   // Wait for the page to load
-  await page.waitForTimeout(TIMEOUT * 5);
+  await page.waitForTimeout(TIMEOUT * 10);
 
   // Submit the vote form
   await page.click('.btn-submit-vote', { delay: randomClickDelay() });
