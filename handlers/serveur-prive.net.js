110b21f3f02fe4f2ca480b560ab11fb99fb5eae2
MRakox <rakoxwastaken@gmail.com>
2022-08-01 18:18:27 +0200

    ♻️ Refactor child process management

--- handlers/serveur-prive.net.js
+++ handlers/serveur-prive.net.js
@@ -10,24 +10,28 @@ export default async function handle(page, { job }) {
 
   // Spawn a new instance of hCaptcha challenger
   const handler = spawn(process.env.CAPTCHA_CHALLENGER_ENTRYPOINT, [
-    join(process.env.CAPTCHA_CHALLENGER_PATH, 'src', 'worker.py'),
-    process.env.MINECRAFT_USERNAME,
+    join(process.env.CAPTCHA_CHALLENGER_PATH, 'src', 'main.py'),
+    'vote',
+    '--username', process.env.MINECRAFT_USERNAME,
+    '--site', 'serveur-prive.net',
   ], {
     cwd: join(process.env.CAPTCHA_CHALLENGER_PATH, 'src'),
-    stdio: 'pipe', // Pipe the output to the parent process
   });
 
   // Wait for the captcha to be solved
   // eslint-disable-next-line no-shadow
   const solve = () => new Promise((resolve, reject) => {
     handler.stdout.on('data', async (data) => {
+      // Retrieve the message from the subprocess
       console.log(data.toString());
       const message = stripAnsi(data.toString());
       await job.log(message);
+      // If the message is a success, resolve the promise
       const result = message.includes('RESULT:') && message.split('RESULT:')[1];
       if (result) resolve(result);
     });
 
+    // If the subprocess times out, reject the promise
     handler.on('error', console.error);
     handler.stderr.on('data', (data) => console.error(data.toString()));
     handler.on('exit', (code) => setTimeout(() => reject(new Error(`The vote handler exited with code ${code}`)), TIMEOUT));
