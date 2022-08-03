85291f8ab7f71dd5de72c1cb16274a00404bbb70
MRakox <rakoxwastaken@gmail.com>
2022-08-03 16:31:26 +0200

    🎨 Improve server structure

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
 import { FastifyAdapter } from '@bull-board/fastify';
 import { QueueScheduler, Queue } from 'bullmq';
 import fastify from 'fastify';
+
 import { QUEUE_NAME, REDIS_CONNECTION } from './utils/constants.js';
 
+// Initialize BullMQ queue and scheduler
 export const scheduler = new QueueScheduler(QUEUE_NAME, REDIS_CONNECTION);
 export const queue = new Queue(QUEUE_NAME, REDIS_CONNECTION);
 
+// Initialize Fastify server
 const app = fastify();
 const serverAdapter = new FastifyAdapter();
 
+// Initialize BullBoard API
 createBullBoard({
   queues: [new BullMQAdapter(queue)],
   serverAdapter,
 });
 
-app
-  .register(serverAdapter.registerPlugin())
+// Register BullBoard API routes & start the server
+app.register(serverAdapter.registerPlugin())
   .listen({ host: '0.0.0.0', port: process.env.SERVER_PORT || 7777 })
   .then((adress) => console.log(`🚀 Server ready at ${adress}`));
